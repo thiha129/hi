3ebc5fe1f088d6fbea63600941db9203a04d28a7
truongtuan04042001 <0404tuantruong@gmail.com>
2020-10-12 20:34:41 +0700

    Revert "haizz"
    This reverts commit f2292a655934f3b4bb1c357b36c040c8e2c3dba8.
    
    # Conflicts:
    #	app/src/main/java/com/example/test1/ThemGiayActivity.java

--- app/src/main/java/com/example/test1/ThemGiayActivity.java
+++ app/src/main/java/com/example/test1/ThemGiayActivity.java
@@ -1,6 +1,9 @@
 package com.example.test1;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
@@ -11,38 +14,31 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.test1.ui.home.HomeFragment;
-
-import org.w3c.dom.Text;
+import com.example.test1.ui.home.HomeViewModel;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 
 public class ThemGiayActivity extends AppCompatActivity {
-    public DatabaseHelper databaseHelper;
+    private DatabaseHelper databaseHelper;
+    private HomeViewModel homeViewModel;
     public static ArrayList<Giay> arrayDoVat;
     public static GiayAdapter adapter;
     Button btnthem2, btnhuy2;
-    EditText Ten, Gia, SoLuong;
+    EditText Ten, Gia,SoLuong;
     EditText etURL;
     Button btnClear, btnSubmit;
     ImageView ivResult;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_them_giay);
-
-        btnClear = findViewById(R.id.btn_clear);
-        btnSubmit = findViewById(R.id.btn_submit);
-        etURL = findViewById(R.id.et_ulr);
-        ivResult = findViewById(R.id.iv_result);
-        Ten = findViewById(R.id.tensp);
-        Gia = findViewById(R.id.giasp);
-        SoLuong = findViewById(R.id.soluongsp);
+        btnClear=findViewById(R.id.btn_clear);
+        btnSubmit=findViewById(R.id.btn_submit);
+        etURL=findViewById(R.id.et_ulr);
+        ivResult=findViewById(R.id.iv_result);
         btnClear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -54,44 +50,53 @@ public class ThemGiayActivity extends AppCompatActivity {
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String urlLink = etURL.getText().toString();
-                if (urlLink.isEmpty()) {
+                String urlLink=etURL.getText().toString();
+                if (urlLink.isEmpty()){
                     Toast.makeText(ThemGiayActivity.this, "Please enter url", Toast.LENGTH_SHORT).show();
-                } else {
-                    LoadImage loadImages = new LoadImage(ivResult);
+                }else {
+                    LoadImage loadImages=new LoadImage(ivResult);
                     loadImages.execute(urlLink);
                 }
             }
         });
-        btnthem2 = findViewById(R.id.btnthemsp);
+        btnthem2=findViewById(R.id.btnthemsp);
         btnthem2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(ThemGiayActivity.this, ""+ Ten.getText().toString(), Toast.LENGTH_SHORT).show();
+                Ten = findViewById(R.id.tensp);
+                Gia = findViewById(R.id.giasp);
+                SoLuong =findViewById(R.id.soluongsp);
+                Ten.getText().toString();
+                Gia.getText().toString();
+                SoLuong.getText().toString();
+                Toast.makeText(ThemGiayActivity.this, "Thêm thành công", Toast.LENGTH_SHORT).show();
+                databaseHelper.UpData("Insert into Sanpham Values(null,'"+Ten.getText().toString()+"','" + Gia.getText().toString() + "','" + SoLuong.getText().toString() + "')");
+                adapter.notifyDataSetChanged();
+                Toast.makeText(ThemGiayActivity.this, "OK! "+Ten.getText().toString(), Toast.LENGTH_SHORT).show();
             }
         });
-        btnhuy2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(ThemGiayActivity.this, MainActivity.class));
-            }
-        });
+//        btnhuy2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent=new Intent(ThemGiayActivity.this, MainActivity.class);
+//                startActivity(intent);
+//            }
+//        });
     }
-
-    private class LoadImage extends AsyncTask<String, Void, Bitmap> {
+    private class LoadImage extends AsyncTask<String,Void, Bitmap> {
         ImageView imageView;
 
         public LoadImage(ImageView ivResult) {
-            this.imageView = ivResult;
+            this.imageView=ivResult;
         }
 
         @Override
         protected Bitmap doInBackground(String... strings) {
-            String urlLink = strings[0];
-            Bitmap bitmap = null;
+            String urlLink=strings[0];
+            Bitmap bitmap=null;
             try {
-                InputStream inputStream = new java.net.URL(urlLink).openStream();
-                bitmap = BitmapFactory.decodeStream(inputStream);
+                InputStream inputStream=new java.net.URL(urlLink).openStream();
+                bitmap= BitmapFactory.decodeStream(inputStream);
 
             } catch (IOException e) {
                 e.printStackTrace();
