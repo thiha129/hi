4a88434b025dbf2144363987b0b83134cfa0d3ee
thiha129 <thim00549@gmail.com>
2020-10-17 23:01:13 +0700

    thoongtinsp_2

--- app/src/main/java/com/example/test1/MainActivityThongTinSanPham.java
+++ app/src/main/java/com/example/test1/MainActivityThongTinSanPham.java
@@ -1,14 +1,92 @@
 package com.example.test1;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 public class MainActivityThongTinSanPham extends AppCompatActivity {
+    TextView name, nd, race, number, test;
+    ImageView imageView1;
+    public DatabaseHelper databaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_thong_tin_san_pham);
+        databaseHelper = new DatabaseHelper(MainActivityThongTinSanPham.this, "giaydep", null, 1);
+        databaseHelper.UpData("CREATE TABLE IF NOT EXISTS Sanpham1(Id INTEGER PRIMARY KEY AUTOINCREMENT,Ten VarChar(150), Gia VarChar(150), SoLuong VarChar(150), LinkAnh VarChar(150),Chitiet VarChar(150))");
+
+        AnhXa();
     }
+    private class LoadImage extends AsyncTask<String, Void, Bitmap> {
+        ImageView imageView;
+
+        public LoadImage(ImageView ivResult) {
+            this.imageView = ivResult;
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            String urlLink = strings[0];
+            Bitmap bitmap = null;
+            try {
+                InputStream inputStream = new java.net.URL(urlLink).openStream();
+                bitmap = BitmapFactory.decodeStream(inputStream);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return bitmap;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            imageView1.setImageBitmap(bitmap);
+            super.onPostExecute(bitmap);
+        }
+    }
+
+    private void AnhXa() {
+        name = findViewById(R.id.tenSanPham);
+        race = findViewById(R.id.GiaSanPham);
+        number = findViewById(R.id.SoLuongSanPham);
+        nd = findViewById(R.id.ChiTietSanPham);
+        imageView1 = findViewById(R.id.imageView3);
+        test = findViewById(R.id.testlink);
+        Intent i = getIntent();
+        int Id = i.getIntExtra("id", 1);
+        Cursor cursor = databaseHelper.GetData("SELECT * FROM Sanpham1 where Id = " + Id + "");
+        while (cursor.moveToNext()) {
+            if (cursor.getInt(0) == Id) {
+                String Name = cursor.getString(1);
+                name.setText(Name);
+                String Race = cursor.getString(2);
+                race.setText(Race);
+                String Number = cursor.getString(3);
+                number.setText(Number);
+                String link = cursor.getString(4);
+//                test.setText(link);
+
+                MainActivityThongTinSanPham.LoadImage loadImages = new MainActivityThongTinSanPham.LoadImage(imageView1);
+                loadImages.execute(link);
+                String ND = cursor.getString(5);
+                nd.setText(ND);
+
+            }
+        }
+
+    }
+
+
+
 }
